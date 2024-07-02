882e87f1ad33553240b55098710ebcd8e1ebdcff
Jose Annunziato <jannunzi@gmail.com>
2024-07-01 21:04:26 -0400

    first commit

---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,7 @@
+export default function Lab1() {
+  return (
+    <div id="wd-lab1">
+      <h2>Lab 1</h2>
+    </div>
+  );
+}
---
+++ src/Labs/index.tsx
@@ -0,0 +1,9 @@
+import Lab1 from "./Lab1";
+export default function Labs() {
+  return (
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <Lab1 />
+    </div>
+  );
+}
